9ce7d2fb4f9ce08da5dd1132f5d9fca808ae8be2
Ayishaakhan786 <ayeshakhan228276@gmail.com>
2024-12-26 00:18:21 +0500

    new code

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -1,14 +1,23 @@
-export default function Footer (){
-    return (
-        <footer className="bg-gray-900 text-gray-300 py-6 px-16 font-sans tracking-wide">
-      <div className="flex justify-between items-center max-lg:flex-col text-center flex-wrap gap-4">
-        <p className="text-[15px] leading-loose">© AK SoftTech. All rights reserved.</p>
-
-        <ul className="flex space-x-6 gap-y-2 max-lg:justify-center flex-wrap">
-          <li><a href="javascript:void(0)" className="text-[15px] hover:text-white">Terms of Service</a></li>
-          <li><a href="javascript:void(0)" className="text-[15px] hover:text-white">Privacy Policy</a></li>
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-6 px-4 sm:px-16 font-sans tracking-wide">
+      <div className="flex flex-col sm:flex-row justify-between items-center text-center sm:text-left flex-wrap gap-4">
+        <p className="text-[15px] leading-loose">
+          © AK SoftTech. All rights reserved.
+        </p>
+        <ul className="flex flex-col sm:flex-row space-x-0 sm:space-x-6 gap-y-2 sm:gap-y-0 sm:justify-start justify-center">
+          <li>
+            <a href="javascript:void(0)" className="text-[15px] hover:text-white">
+              Terms of Service
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)" className="text-[15px] hover:text-white">
+              Privacy Policy
+            </a>
+          </li>
         </ul>
       </div>
     </footer>
-    );
+  );
 }
